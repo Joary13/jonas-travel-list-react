a3b5ecb2484d4b6a14b13439deca14bf453a96bc
Joary13 <jolea13008@gmail.com>
2023-11-27 10:17:08 +0100

    lift up items state to be used by Packing list

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className='app'>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -22,7 +27,7 @@ function Logo() {
   return <h1>🌴 Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
 
@@ -31,6 +36,7 @@ function Form() {
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+    onAddItems(newItem);
     setDescription('');
     setQuantity(1);
   }
@@ -59,11 +65,12 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
+  console.log(items);
   return (
     <div className='list'>
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
